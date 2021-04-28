e6996efb223eab9582e4b65a399b79b7e2403ece
Ruan Miguel <ruanmiguelps@gmail.com>
2021-04-28 08:39:04 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DROP VIEW estatisticas_musicais;
+CREATE VIEW estatisticas_musicais AS 
+SELECT 
+    COUNT(*) AS cancoes,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.artistas) AS artistas,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.album) AS albuns
+FROM
+    SpotifyClone.cancoes;
