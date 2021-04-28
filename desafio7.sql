b107ef8e24fc456cf2f3c3fbd1b65a40a381e2b8
Ruan Miguel <ruanmiguelps@gmail.com>
2021-04-28 12:48:33 -0300

    desafio7 ;
    0

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+CREATE VIEW perfil_artistas AS 
+SELECT 
+    A.artistas_name AS artista,
+    AL.album_name AS album,
+    COUNT(S.seguindo_name) AS seguidores
+FROM
+    SpotifyClone.artistas AS A
+        INNER JOIN
+    SpotifyClone.album AS AL ON AL.artistas_id = A.id
+        INNER JOIN
+    SpotifyClone.seguindo_artistas AS S ON S.artistas_id = AL.artistas_id
+GROUP BY album , artista
+ORDER BY seguidores DESC ,
+artista ASC , 
+album ASC;
