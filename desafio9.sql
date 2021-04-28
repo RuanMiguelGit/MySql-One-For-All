6246715fa06867c7f3b42c24e1b289b827a96ed8
Ruan Miguel <ruanmiguelps@gmail.com>
2021-04-28 19:54:22 -0300

    req 9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN aname VARCHAR(100))
+BEGIN
+SELECT A.artistas_name AS artista, AL.album_name AS album 
+FROM SpotifyClone.artistas AS A
+INNER JOIN SpotifyClone.album AS AL
+ON A.id = AL.artistas_id
+WHERE A.artistas_name = aname;
+END $$
+DELIMITER ;
