47fe0c50651b1a746469ebf5309f0ac86143349d
Ruan Miguel <ruanmiguelps@gmail.com>
2021-04-28 19:43:52 -0300

    req 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,8 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON SpotifyClone.usuario_dados
     FOR EACH ROW
 BEGIN
-    DELETE FROM SpotifyClone.historico_cancoes AS HC
-	WHERE HC.usuario_dados_id = OLD.HC.usuario_dados_id;
+DELETE FROM SpotifyClone.historico_cancoes AS HC
+WHERE HC.usuario_dados_id = OLD.HC.usuario_dados_id;
 DELETE FROM SpotifyClone.seguindo_artistas AS A 
 WHERE
     A.usuario_dados_id = OLD.A.usuario_dados_id;
